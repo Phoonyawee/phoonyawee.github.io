105c4d7dc7abd985f569542b50c9b7150b8327ff
Phoonyawee <punyaweenamwong@gmail.com>
2026-05-31 17:16:13 +0700

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,18 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: "ERP Developer",
+    company: "Thai Nippon Rubber Industry — Pintong Branch",
+    duration: "May 2024 - Aug 2025",
+    description:
+      "Designed, developed, and customized web interfaces to align with user requirements within the company's ERP system. Collaborated with internal teams to gather requirements and deliver functional web-based solutions.",
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Final Project — Computer Repair Notification System",
+    company: "Burapha University",
+    duration: "Nov 2022 - May 2023",
+    description:
+      "Developed a notification system for tracking and managing computer repair requests within the university. Responsible for both system design and implementation as part of the undergraduate capstone project.",
   },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
-  }
-]
+];
