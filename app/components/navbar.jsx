1b74ffef7c6cb2ddb24be90ebe038ca46f83766b
Phoonyawee <punyaweenamwong@gmail.com>
2026-05-31 17:56:15 +0700

    Update navbar.jsx

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,7 +1,5 @@
 // @flow strict
 import Link from "next/link";
-
-
 function Navbar() {
   return (
     <nav className="bg-transparent">
@@ -13,7 +11,6 @@ function Navbar() {
             Phoonyawee
           </Link>
         </div>
-
         <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100" id="navbar-default">
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#about">
@@ -30,7 +27,7 @@ function Navbar() {
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/blog"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">BLOGS</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#products"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PRODUCTS</div></Link>
           </li>
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
@@ -40,5 +37,4 @@ function Navbar() {
     </nav>
   );
 };
-
 export default Navbar;
