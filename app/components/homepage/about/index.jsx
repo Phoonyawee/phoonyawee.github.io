668733ea7eb2a2d01847c6e6a31476e0909002ba
Phoonyawee <punyaweenamwong@gmail.com>
2026-09-04 22:04:44 +0700

    Update profile photo

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -26,7 +26,7 @@ function AboutSection() {
           <Image
             src={personalData.profile}
             width={280}
-            height={280}
+            height={350}
             alt="Phoonyawee Namwong"
             className="rounded-lg transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"
           />
@@ -37,3 +37,4 @@ function AboutSection() {
 };
 
 export default AboutSection;
+
